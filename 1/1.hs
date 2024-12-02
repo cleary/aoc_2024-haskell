2ac6fe1ab0cdd376432834d74ae046edd9ebea78
Bernard Gray <bernard.gray@gmail.com>
2024-12-02 16:40:46 +1100

    1 solved

--- 1/1.hs
+++ 1/1.hs
@@ -1,11 +1,20 @@
 import System.IO
-import Control.Monad
+import Data.List
+
+-- add numbers in a list
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
 
 main = do
         inputFile <- openFile "./input" ReadMode
         contents <- hGetContents inputFile
-        lines contents
+        let splitWords = [ words x | x <- lines contents ];
+        let left = [ head x | x <- splitWords ];
+        let right = [ last x | x <- splitWords ];
+        -- sort and convert Str to Int
+        let sortLeft = map (read::String->Int) (sort left);
+        let sortRight = map (read::String->Int) (sort right);
+        let subtractList = [ if x > y then x - y else y - x | (x,y) <- zip sortLeft sortRight ];
+        print (sumList subtractList);
         hClose inputFile
-
-f :: [String] -> [Int]
-f = map read
