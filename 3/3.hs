0f3fe526407660158a7cb1c2a8db102241bdcdfd
Bernard Gray <bernard.gray@gmail.com>
2024-12-05 18:07:12 +1100

    day 3 pt 1 done

--- 3/3.hs
+++ 3/3.hs
@@ -1,13 +1,26 @@
 import System.IO
 import Data.List
+import Data.Text
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
+import Text.Read (readMaybe)
+
+parseTuple :: String -> Maybe (Int, Int)
+parseTuple s = readMaybe s :: Maybe (Int, Int)
+
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (x:xs) = x + sumList xs
 
 main = do
         inputFile <- openFile "./input" ReadMode
         contents <- hGetContents inputFile
-        let output = [ x =~ "mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: [[String]] | x <- lines contents ];
-        -- let muls = [ x =~ "[0-9]{1,3},[0-9]{1,3}" :: [[String]] | x <- output ];
-        print output;
-
-        -- let output = [ x =~ "mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: [[String]] | x <- lines contents ];
+        -- concat multiple times to flatten lists
+        let muls = Data.List.concat (Data.List.concat ([ x =~ "mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: [[String]] | x <- Data.List.lines contents ]));
+        -- remove 'mul' from string and convert remainder to (Int, Int) tuple
+        let  valPairs = [case parseTuple (unpack (replace (pack "mul") (pack "") ( pack x) )) of 
+                          Just t -> t
+                        | x <- muls ]
+        -- muliply the pairs
+        let mulVal = [ x * y | (x,y) <- valPairs ]
+        print (sumList mulVal);
--- 3/notes.md
+++ 3/notes.md
@@ -1,5 +1,6 @@
 Good:
 https://williamyaoh.com/posts/2019-04-11-cheatsheet-to-regexes-in-haskell.html
+https://forkful.ai/en/haskell/strings/searching-and-replacing-text/
 
 Junk:
 https://lotz84.github.io/haskellbyexample/ex/regular-expressions
