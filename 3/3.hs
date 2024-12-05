5730e9c859813eb97d65c9fe4f54e8fe43a804a8
Bernard Gray <bernard.gray@gmail.com>
2024-12-05 13:30:40 +1100

    multi returns working, WIP

--- 3/3.hs
+++ 3/3.hs
@@ -6,5 +6,8 @@ import Text.Regex.TDFA.Text ()
 main = do
         inputFile <- openFile "./input" ReadMode
         contents <- hGetContents inputFile
-        let output = [ x =~ "mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: String | x <- lines contents ];
+        let output = [ x =~ "mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: [[String]] | x <- lines contents ];
+        -- let muls = [ x =~ "[0-9]{1,3},[0-9]{1,3}" :: [[String]] | x <- output ];
         print output;
+
+        -- let output = [ x =~ "mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: [[String]] | x <- lines contents ];
