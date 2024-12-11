0644d8ec347eec5ad1b68818cb1b6d4843a7c993
Bernard Gray <bernard.gray@gmail.com>
2024-12-11 14:49:38 +1100

    day 3pt2 finished

--- 3/3pt2.hs
+++ 3/3pt2.hs
@@ -13,36 +13,30 @@ sumList :: [Int] -> Int
 sumList [] = 0
 sumList (x:xs) = x + sumList xs
 
--- splitAtDo :: [a] -> [a]
-splitAtDo :: [Text] -> [[Text]]
+-- splitAtDo :: [String] -> [String]
+splitAtDo :: [String] -> [String]
 splitAtDo [] = []
-splitAtDo xs = (discard, rest) = Data.List.splitAt (fromMaybe 0 (elemIndex "do()" xs)) xs
-                 if length rest == 0 
-                 then [] 
-                 else (splitAtDont rest)
+splitAtDo xs = case (Data.List.splitAt (fromMaybe 0 (elemIndex "do()" xs))) xs of
+                 ([], _) -> []
+                 (discard, rest) -> splitAtDont (Data.List.tail rest)
 
-splitAtDont :: [Text] -> [[Text]]
+splitAtDont :: [String] -> [String]
 splitAtDont [] = []
-splitAtDont xs =   index <- fromMaybe 0 (elemIndex "do()" xs)
-                   if index == 0
-                   then xs
-                   else (keep, rest) <- Data.List.splitAt index xs
-                     keep ++ (splitAtDo rest) 
+splitAtDont xs = case (Data.List.splitAt (fromMaybe 0 (elemIndex "don't()" xs))) xs of
+                   ([], keep) -> keep
+                   (keep, rest) -> keep ++ (splitAtDo (Data.List.tail rest))
 
 
 main = do
         inputFile <- openFile "./input" ReadMode
         contents <- hGetContents inputFile
         -- concat multiple times to flatten lists
-        let muls = Data.List.concat (Data.List.concat ([ x =~ "(don't[(][)]|do[(][)]|mul[(][0-9]{1,3},[0-9]{1,3}[)])" :: [[String]] | x <- Data.List.lines contents ]));
-        print (splitAtDo muls)
-        -- let (headinc, tail) = Data.List.splitAt (fromMaybe 0 (elemIndex "don't()" muls)) muls;
-        -- print (headinc)
-
-        -- -- remove 'mul' from string and convert remainder to (Int, Int) tuple
-        -- let  valPairs = [case parseTuple (unpack (replace (pack "mul") (pack "") ( pack x) )) of 
-        --                   Just t -> t
-        --                 | x <- muls ]
-        -- -- muliply the pairs
-        -- let mulVal = [ x * y | (x,y) <- valPairs ]
-        -- print (sumList mulVal);
+        let muls = Data.List.concat (Data.List.concat ([ x =~ "don't[(][)]|do[(][)]|mul[(][0-9]{1,3},[0-9]{1,3}[)]" :: [[String]] | x <- Data.List.lines contents ]));
+        -- remove 'mul' from string and convert remainder to (Int, Int) tuple
+        let  valPairs = [case parseTuple (unpack (replace (pack "mul") (pack "") ( pack x) )) of 
+                      Just t -> t
+                      Nothing -> (0,0)
+                    | x <- splitAtDont muls ]
+        -- muliply the pairs
+        let mulVal = [ x * y | (x,y) <- valPairs ]
+        print (sumList mulVal);
